6cfd96e1a83df60c4b597a213ca9cf6f23a88231
Axel Ariel Godoy <axelariel78@gmail.com>
2022-06-22 22:59:51 -0300

    sexto commit

--- desafio/models.py
+++ desafio/models.py
@@ -1,7 +1,10 @@
+import string
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class Prueba(models.Model):
     nombre = models.CharField(max_length=30)
-    
+    numero = models.IntegerField()
+    fecha = models.DateField()
--- desafio/views.py
+++ desafio/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader, Template, Context
 from .models import Prueba
-
+from datetime import datetime
 # Create your views here.
 
 def una_vista(request):
@@ -12,13 +12,21 @@ def un_template(request):
     
     template = loader.get_template('index.html')
     
-    prueba1 = Prueba(nombre = 'Azucena')
-    prueba2 = Prueba(nombre = 'Carlos')
-    prueba3 = Prueba(nombre = 'Yamila')
-    
+    prueba1 = Prueba(nombre = 'Azucena',numero = 55,fecha ='1960-12-02')
+    prueba2 = Prueba(nombre = 'Carlos', numero = 60,fecha ='1950-05-05')
+    prueba3 = Prueba(nombre = 'Yamila', numero = 30,fecha ='2000-09-11')
+    # prueba4 = Prueba(numero = 55)
+    # prueba5 = Prueba(fecha = 22/22/22)
+    # fecha = datetime.date(22/22/22)
     prueba1.save()
     prueba2.save()
     prueba3.save()
+    # prueba4.save()
+    # prueba5.save()
+    
     Prueba.objects.all()
-    documento = template.render({'lista_objetos': [prueba1,prueba2,prueba3]})
+    
+    
+    documento = template.render({'lista_objetos': [prueba1, prueba2, prueba3]})
+    
     return HttpResponse(documento)
